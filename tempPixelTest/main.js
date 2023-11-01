ed538bba05ac7c16f324a14dbe41b30f12480369
BurntWaffleCake <neondenetro@gmail.com>
2023-11-01 17:24:02 -0500

    resolution test

--- tempPixelTest/main.js
+++ tempPixelTest/main.js
@@ -1,11 +1,19 @@
 const source = document.getElementById("source");
 const ctx = source.getContext("2d");
 
-function render(ctx, dt) {
+let resolution = 20;
+
+function render(ctx, dt, t) {
   for (let x = 0; x < ctx.canvas.width; x++) {
     for (let y = 0; y < ctx.canvas.height; y++) {
       ctx.fillStyle =
-        "rgba(" + 255 * Math.sin(x / 50) + "," + 0 + "," + 255 * Math.sin(y / 50) + ")";
+        "rgba(" +
+        255 * Math.sin((x * resolution) / 50 + t) +
+        "," +
+        String(0) + // * Math.sin((x * resolution) / 50 + t) +
+        "," +
+        String(0) + // * Math.sin((y * resolution) / 50 + t) +
+        ")";
       ctx.fillRect(x, y, 1, 1);
     }
   }
@@ -18,18 +26,24 @@ function loop(time) {
   let dt = time / 1000 - t;
   t = time / 1000;
 
+  console.log(dt);
+
   updateCanvasSize(ctx);
-  render(ctx, dt);
-  //   window.requestAnimationFrame(loop);
+  render(ctx, dt, t);
+  ctx.fillStyle = "rgb(255,255,255)";
+  ctx.fillRect(ctx.canvas.width / 2, ctx.canvas.height / 2, 5, 5);
+  console.log(ctx.canvas.width * ctx.canvas.height);
+  window.requestAnimationFrame(loop);
 }
 
 function init() {
   window.requestAnimationFrame(loop);
 }
 
+ctx.imageSmoothingEnabled = false;
 init();
 
 function updateCanvasSize(ctx) {
-  ctx.canvas.width = source.clientWidth;
-  ctx.canvas.height = source.clientHeight;
+  ctx.canvas.width = source.clientWidth / resolution;
+  ctx.canvas.height = source.clientHeight / resolution;
 }
